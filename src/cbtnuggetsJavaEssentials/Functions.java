0f4494eeba7e3051be76540481433aa14d82e5fd
bmihovski <bmihovski@live.com>
2018-03-08 18:46:57 +0200

    Squashed commit of the following:
    commit 9bc984af418e16339e985842c3d2132ef1787b8e
    Author: bmihovski <bmihovski@live.com>
    Date:   Thu Mar 08 18:46:42 2018 +0200
    
    	11 functions tested

---
+++ src/cbtnuggetsJavaEssentials/Functions.java
@@ -0,0 +1,20 @@
+package cbtnuggetsJavaEssentials;
+
+public class Functions {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Before function call");
+		myCoolFunction();
+		System.out.println("After function call");
+		
+	}
+	
+	public static void myCoolFunction() {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Ben");
+		
+	}
+
+}

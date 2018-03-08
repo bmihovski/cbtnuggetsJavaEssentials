277bc69428b8cc4e6b357b512f7c8f57a2223485
bmihovski <bmihovski@live.com>
2018-03-08 17:38:41 +0200

    Squashed commit of the following:
    commit 8ab67e4826521d486fa6e69b155c22f78d5ac42c
    Author: bmihovski <bmihovski@live.com>
    Date:   Thu Mar 08 17:38:25 2018 +0200
    
    	09 loops tested

---
+++ src/cbtnuggetsJavaEssentials/IsPrime.java
@@ -0,0 +1,28 @@
+package cbtnuggetsJavaEssentials;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		
+		int number = 57;
+		boolean isPrime = true;
+		
+		for (int test = 2; test < number; test ++) {
+			if (number % test == 0) {
+				isPrime = false;
+				System.out.println(number + " is divisible by " + test);
+				break;
+			}
+			System.out.println(test);
+		}
+		
+		if (isPrime) {
+			System.out.println("This is a prime number!");
+		}
+		else {
+			System.out.println("This is not a prime number!");
+		}
+		
+	}
+
+}

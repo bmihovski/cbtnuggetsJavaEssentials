1c44bc5ecce197a076fb6e7f44e36dfe6174c0fb
bmihovski <bmihovski@live.com>
2018-03-10 13:27:51 +0200

    Squashed commit of the following:
    commit b6570c539eb2ac9e42c623ea63deabd2d70fc28e
    Author: bmihovski <bmihovski@live.com>
    Date:   Sat Mar 10 13:27:35 2018 +0200
    
    	15 package imports tested.

---
+++ src/cbtnuggetsJavaEssentials/PkgImports.java
@@ -0,0 +1,17 @@
+package cbtnuggetsJavaEssentials;
+import java.lang.Math;
+
+public class PkgImports {
+
+	public static void main(String[] args) {
+		
+		int x = 25;
+		double result;
+		
+		result = Math.sqrt(x);
+		
+		System.out.println(result);
+
+	}
+
+}
